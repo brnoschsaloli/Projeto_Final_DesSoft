0bc691564bb997fbd55976efc0e261ad78df8b0f
brnoschsaloli <brnoschsaloli@gmail.com>
2022-05-18 14:01:41 -0300

    imagens

--- Jogo.py
+++ Jogo.py
@@ -2,10 +2,13 @@ import pygame
 
 pygame.init()
 
-window = pygame.display.set_mode((600, 750))
+window = pygame.display.set_mode((570, 600))
 pygame.display.set_caption('Rápidos e muito bravos')
 
 jogo = True
+
+image = pygame.image.load('assets/Images/rua2.png').convert()
+image = pygame.transform.scale(image, (570, 870))
 
 while jogo:
 
@@ -14,7 +17,7 @@ while jogo:
             jogo = False
 
     window.fill((255, 255, 255))
-
+    window.blit(image, (0, 0))
     pygame.display.update() 
 
 pygame.quit()  
